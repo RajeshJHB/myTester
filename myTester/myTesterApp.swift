7f121cb6a2be6b57c76e1cc71e0b42b1a5a4d118
rajesh <rajeshgotmail@gmail.com>
2025-09-19 15:53:05 -0700

    Initial Commit

---
+++ myTester/myTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTesterApp.swift
+//  myTester
+//
+//  Created by Rajesh Solanki on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct myTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myTesterTests/myTesterTests.swift
@@ -0,0 +1,17 @@
+//
+//  myTesterTests.swift
+//  myTesterTests
+//
+//  Created by Rajesh Solanki on 9/19/25.
+//
+
+import Testing
+@testable import myTester
+
+struct myTesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
